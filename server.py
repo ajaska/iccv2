319cd5117ce99ac2295665da4a8eba19f110fd87
Arlan Jaska <ajaska@uber.com>
2016-07-19 19:27:59 -0700

    Implement auth

--- server.py
+++ server.py
@@ -4,15 +4,35 @@ import requests
 from flask import (
     Flask,
     jsonify,
+    redirect,
     request,
+    session,
+    url_for,
 )
 app = Flask(__name__)
+
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+app.secret_key = config['Server']['SecretKey']
+
+ig_client_id = config['Instagram']['ClientId']
+ig_client_secret = config['Instagram']['ClientSecret']
+redirect_uri = 'http://' + config['Server']['Hostname'] + '/auth'
+
 @app.route('/')
-def hello_world():
+def index():
     return 'Hello, World!'
+
+@app.route('/auth-redirect')
+def oauth_redirect():
+    redirect_url = (
+        'https://api.instagram.com/oauth/authorize/?'
+        'client_id={}&'
+        'redirect_uri={}&'
+        'response_type=code'
+    ).format(ig_client_id, redirect_uri)
+    return redirect(redirect_url, code=302)
 
 @app.route('/auth')
 def oauth_callback():
@@ -23,11 +43,11 @@ def oauth_callback():
         return 'Error: missing code parameter', 400
 
     payload = {
-        'client_id': config['Instagram']['ClientId'],
-        'client_secret': config['Instagram']['ClientSecret'],
+        'client_id': ig_client_id,
+        'client_secret': ig_client_secret,
         'grant_type': 'authorization_code',
         'code': code,
-        'redirect_uri': config['Server']['Hostname'] + "/auth",
+        'redirect_uri': redirect_uri,
     }
 
     r = requests.post(
@@ -35,4 +55,9 @@ def oauth_callback():
         data=payload
     )
 
-    return jsonify(**r.json())
+    if r.status_code != 200:
+        return jsonify(**r.json()), r.status_code
+
+    data = r.json()
+    session['access_token'] = data.get('access_token')
+    return redirect(url_for('index'))
